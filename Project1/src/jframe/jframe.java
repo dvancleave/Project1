7598080e5e3450d468552fbba4791d139826ba74
uyuyuy99 <uyuyuy99@gmail.com>
2017-10-04 15:12:07 -0700

    increased window size (again)

--- Project1/src/jframe/jframe.java
+++ Project1/src/jframe/jframe.java
@@ -12,7 +12,7 @@ public class jframe {
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JLabel emptyLabel = new JLabel("");
-		emptyLabel.setPreferredSize(new Dimension(500, 200));
+		emptyLabel.setPreferredSize(new Dimension(500, 201));
 		
 		frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
 		
